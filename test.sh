91cfb2f946490829074c543de584906f819edeb8
baota <644727408@qq.com>
2024-05-28 01:30:02 +0800

    自动提交 Tue May 28 01:30:02 CST 2024

--- test.sh
+++
@@ -1,7 +0,0 @@
-all_backups=($(ls))
-backup_files=($(ls -t | head -n 10))
-for file in "${all_backups[@]}"; do
-    if [[ ! " ${backup_files[@]} " =~ " $file " ]]; then
-        echo "$file"
-    fi
-done
